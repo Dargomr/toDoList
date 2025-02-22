b7ae02a964fb3d1d8addac234b958d4c205441a8
jpg <Shoot-nik4@yandex.ru>
2025-02-22 13:35:26 +0300

    todo

---
+++ assets/js/scripts.js
@@ -0,0 +1,152 @@
+const list = document.querySelectorAll("li");
+list.forEach(async (el) => {
+    el.addEventListener("click", async (ev) => {
+      if (ev.target.classList.contains('li__checkbox') == false) {
+        el.classList.toggle("done")
+        let liDataDone = {
+          done: el.classList.contains('done'),
+          uuid: el.dataset.uuid,
+        }
+
+        let response = await fetch('/done', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(liDataDone),
+        })
+
+      }
+      
+    });
+
+
+
+
+    // let response = await fetch('/checkCreate', {
+    //   method: "POST",
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({uuid: el.dataset.uuid}),
+    // })
+
+    // response = await response.json();
+    // console.log(response);
+    // checkbox.checked = response.checked;
+})
+
+const checkboxes = document.querySelectorAll('.li__checkbox')
+checkboxes.forEach((el) => {
+  el.addEventListener('click', async (ev) => {
+    liDataCheck = {
+      checked: el.checked,
+      uuid: el.parentNode.dataset.uuid,
+    }
+    console.log(liDataCheck);
+    let response = await fetch('/check', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(liDataCheck),
+    })
+  })
+})
+
+const deleteButtons = document.querySelectorAll('.delete-button')
+deleteButtons.forEach((el) => {
+  el.addEventListener('click', async (ev) => {
+    el.parentNode.remove();
+    console.log(el.parentNode.dataset.uuid);
+    let response = await fetch('/delete', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({uuid: el.parentNode.dataset.uuid}),
+      })
+  })
+})
+
+const addLiButton = document.querySelector('.add-li-button');
+const toDoUl = document.querySelector('.to-do__ul');
+const inputField = document.querySelector('.input-field');
+const errorMsg = document.querySelector('.error-msg');
+
+inputAdd.onsubmit = async (e) => {
+  e.preventDefault();
+
+  if (inputField.value != '') {
+    const liItem = document.createElement('li');
+    const checkbox = createCheckbox();
+    liItem.appendChild(checkbox);
+    liItem.append(inputField.value);
+    toDoUl.appendChild(liItem);
+    liItem.addEventListener("click", (ev) => {
+      if (!ev.target.classList.contains('li__checkbox')) {
+        liItem.classList.toggle("done")
+      }
+      
+    });
+    errorMsg.classList.remove('error-msg_active');
+    
+    let response = await fetch('/add',  {
+      method: 'POST',
+      body: new FormData(inputAdd),
+    })
+    response = await response.json();
+    liItem.dataset.uuid = response.uuid
+
+
+  } else {
+    errorMsg.classList.add('error-msg_active');
+  }
+}
+
+function createCheckbox() {
+  const checkout = document.createElement('input');
+  checkout.type = 'checkbox';
+  checkout.classList.add('li__checkbox')
+  return checkout;
+
+}
+
+const deleteAllChecked = document.querySelector('.delete-all-checked');
+const completeAllChecked = document.querySelector('.complete-all-checked');
+
+const listCheckBoxes = document.getElementsByClassName("li__checkbox");
+
+deleteAllChecked.addEventListener("click", async () => {
+  let mas = [];
+  Array.from(document.getElementsByClassName("li__checkbox")).forEach((el) => {
+    if (el.checked) {
+      mas.push(el.parentNode.dataset.uuid)
+      el.parentNode.remove();
+    }
+  })
+  console.log(mas)
+  let response = await fetch('/deleteAll', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(mas),
+  })
+})
+
+completeAllChecked.addEventListener('click', async () => {
+  let mas = [];
+  Array.from(document.getElementsByClassName('li__checkbox')).forEach((el) => {
+    if(el.checked) {
+      mas.push(el.parentNode.dataset.uuid, el.parentNode.classList.contains('done'))
+      el.parentNode.classList.add('done')
+    }
+  })
+  let response = await fetch('/completeAll', {
+    method: 'POST',
+  })
+})
+
+
+
---
+++ index.js
@@ -0,0 +1,94 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer();
+const {v4 : uuidv4} = require('uuid');
+// const bodyParser = require('body-parser');
+// const { stringify } = require('querystring');
+
+const connectionUrl = 'mongodb://localhost:27017/toDo';
+mongoose.connect(connectionUrl);
+
+const actionSchema = {
+    uuid: String,
+    action: String,
+    checked: Boolean,
+    done: Boolean,
+}
+
+const Action = mongoose.model('actions', actionSchema) 
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('assets'))
+
+app.use(multer().none());
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+// app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/', async function(request, response) {
+    const ulActions = Array.from(await Action.find());
+    const data = {ulActions : ulActions}
+    response.render('main', data);
+})
+
+app.post('/add', function(request, response) {
+    try {
+        // console.debug(request.body)
+        let obj = request.body;
+        let uuid = uuidv4()
+        let res = {
+            uuid: uuid,
+        }
+        let newAction = new Action({
+            uuid: uuid,
+            action: obj.inputField,
+            checked: false,
+            done: false, 
+        })
+
+        // console.log(newAction)
+        newAction.save();
+        response.json(res)
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+app.post('/done', async function(request, response) {
+    await Action.updateOne({uuid: request.body.uuid}, {$set: {done: request.body.done}})
+})
+
+app.post('/check', async function(request, response) {
+    await Action.updateOne({uuid: request.body.uuid}, {$set: {checked: request.body.checked}})
+})
+
+app.post('/checkCreate', async function(request, response) {
+    //найти поле checked по uuid из тела request и вернуть его//
+    let res = await Action.find({uuid: request.body.uuid})
+    // console.log(res[0].checked)
+    response.json(res[0].checked)
+})
+
+app.post('/deleteAll', async function(request, response) {
+    let mass = []
+    await Action.deleteMany({uuid: request.body})
+})
+
+app.post('/completeAll', async function(request, response) {
+    let mass = []
+    await Action.updateMany({checked: true}, {$set: {done: true}})
+})
+
+app.post('/delete', async function(request, response) {
+    await Action.deleteOne({uuid: request.body.uuid})
+})
+
+
+app.listen(4000, () => console.log('/huynya zapustilasb'))
